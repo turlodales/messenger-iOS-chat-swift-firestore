b925896cd49b42fb794ce17b89208a756663dfaf
alexanderkalema <alexanderkalema@gmail.com>
2025-11-26 15:46:16 +0300

    update

---
+++ Core/ViewControllers/Navigation/ATCMenuItemCollectionViewCellProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ATCMenuItemCellProtocol.swift
+//  MagicShop
+//
+//  Created by Florian Marcu on 3/21/18.
+//  Copyright © 2018 Magic Shop. All rights reserved.
+//
+import UIKit
+
+@MainActor
+protocol ATCMenuItemCollectionViewCellProtocol: ATCMenuCollectionViewCellConfigurable {
+    var menuImageView: UIImageView! {get set}
+    var menuLabel: UILabel! {get set}
+}
